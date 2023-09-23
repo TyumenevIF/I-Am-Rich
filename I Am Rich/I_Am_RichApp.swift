428238541c3459f49225d6385e34c5aea3ea8b00
Ilyas Tyumenev <tumenoff@mail.ru>
2023-09-23 18:39:56 +0300

    Initial commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Ilyas Tyumenev on 23.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
